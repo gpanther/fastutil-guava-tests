d7ca85a3e566540ec21d760fe2f4d828fb370c7f
Grey Panther <gpanther@grey-panther.info>
2016-02-21 08:26:58 +0200

    A few tests for BigLists

---
+++ src/test/java/net/greypanther/fastutil/tests/IntBigListTest.java
@@ -0,0 +1,28 @@
+package net.greypanther.fastutil.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import it.unimi.dsi.fastutil.ints.IntBigArrayBigList;
+import it.unimi.dsi.fastutil.ints.IntBigList;
+import it.unimi.dsi.fastutil.ints.IntBigLists;
+
+public final class IntBigListTest {
+  @Test(expected = IndexOutOfBoundsException.class)
+  public void testListIterator_tooLow() {
+    new IntBigArrayBigList().listIterator(-1L);
+  }
+
+  @Test(expected = IndexOutOfBoundsException.class)
+  public void testListIterator_tooHigh() {
+    new IntBigArrayBigList().listIterator(1L);
+  }
+
+  @Test
+  public void testAddWithIterator() {
+    IntBigList list = new IntBigArrayBigList();
+    list.iterator().add(1);
+    assertEquals(IntBigLists.singleton(1), list);
+  }
+}
